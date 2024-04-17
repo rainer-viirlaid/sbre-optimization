8e81f8bb133b624df3fd04cb54baddd67925a015
Rainer Viirlaid <a@a.a>
2024-04-17 18:40:02 +0300

    Created benchmarks for the original baseline commit.

--- src/Sbre.Benchmarks/Optimizations.fs
+++ src/Sbre.Benchmarks/Optimizations.fs
@@ -1,147 +1,204 @@
 module Sbre.Benchmarks.Optimizations
 
+open System.Globalization
 open BenchmarkDotNet.Attributes
+open BenchmarkDotNet.Columns
+open BenchmarkDotNet.Configs
+open BenchmarkDotNet.Diagnosers
+open BenchmarkDotNet.Exporters.Csv
+open BenchmarkDotNet.Reports
+open BenchmarkDotNet.Validators
+open Perfolizer.Horology
 open Sbre
 open Sbre.Benchmarks.Jobs
 open Sbre.Optimizations
 open System
 open Sbre.Pat
 open Sbre.Types
-let fullInput =
-    __SOURCE_DIRECTORY__ + "/data/input-text.txt" |> System.IO.File.ReadAllText
 
-let shortInput20k = fullInput[..19999] // 20k chars limit
-let shortInput10k = fullInput[..9999] // 10k chars limit
+let twain = __SOURCE_DIRECTORY__ + "/data/input-text.txt" |> System.IO.File.ReadAllText
+// let sherlock = __SOURCE_DIRECTORY__ + "/data/sherlock.txt" |> System.IO.File.ReadAllText
+// let rust = __SOURCE_DIRECTORY__ + "/data/rust-src-tools-3b0d4813.txt" |> System.IO.File.ReadAllText
+
+let testInput =
+                // rust
+                twain
+                // sherlock
+                // |> String.replicate 10
+                // |> String.replicate 100
 
 
-[<BenchmarkDotNet.Attributes.MemoryDiagnoser>]
-[<ShortRunJob>]
-[<AbstractClass>]
-type StringPrefix(pattern:string) =
-    let regex = Sbre.Regex(pattern)
-    let matcher = regex.TSetMatcher
-    // find optimized prefix for regex
-    let optimizations =
-        Sbre.Optimizations.findInitialOptimizations
-            (fun node -> matcher.GetOrCreateState(node).Id)
-            (fun node -> matcher.GetOrCreateState(node).Flags)
-            matcher.Cache
-            matcher.ReversePattern
-            matcher.ReverseTrueStarredPattern
-    let prefixString =
-        match optimizations with
-        | InitialOptimizations.StringPrefix(prefix, transitionNodeId) ->
-            prefix
-        | _ -> failwith "could not get prefix"
+module Patterns =
+    
+    // Patterns from Rebar
 
-    let charToTSet (chr:char) = matcher.Cache.CharToMinterm(chr)
-    let isElemOfSet (tset1:TSet) (tset2:TSet) = Solver.elemOfSet tset1 tset2
+    // rust-src-tools-3b0d4813.txt
+    // TODO: bitvector error
+    // let DATE = System.IO.File.ReadAllText(__SOURCE_DIRECTORY__ + "/data/pattern-date.txt" )
+    
+    // rust-src-tools-3b0d4813.txt
+    // let URL = System.IO.File.ReadAllText(__SOURCE_DIRECTORY__ + "/data/pattern-url.txt" )
+    
+    [<Literal>]
+    let SHERLOCK = @"Sherlock Holmes|John Watson|Irene Adler|Inspector Lestrade|Professor Moriarty"
+    
+    [<Literal>]
+    let SHERLOCK_CASEIGNORE = @"(?i)Sherlock Holmes|John Watson|Irene Adler|Inspector Lestrade|Professor Moriarty"
+
+    // Twain patterns from https://zherczeg.github.io/sljit/regex_perf.html
+
+    [<Literal>]
+    let TWAIN = @"Twain"
+
+    [<Literal>]
+    let TWAIN_CASEIGNORE = @"(?i)Twain"
+
+    [<Literal>]
+    let AZ_SHING = @"[a-z]shing"
+
+    [<Literal>]
+    let HUCK_SAW = @"Huck[a-zA-Z]+|Saw[a-zA-Z]+"
+    
+    [<Literal>]
+    let WORD_END = @"\b\w+nn\b" // Similar but differentx: \w+nn\W
+
+    [<Literal>]
+    let AQ_X = @"[a-q][^u-z]{13}x"
+
+    [<Literal>]
+    let TOM_SAWYER_HUCKLEBERRY_FINN = @"Tom|Sawyer|Huckleberry|Finn"
+
+    [<Literal>]
+    let TOM_SAWYER_HUCKLEBERRY_FINN_CASEIGNORE = @"(?i)Tom|Sawyer|Huckleberry|Finn"
+
+    [<Literal>]
+    let D02_TOM_SAWYER_HUCKLEBERRY_FINN = @".{0,2}(Tom|Sawyer|Huckleberry|Finn)"
+
+    [<Literal>]
+    let D24_TOM_SAWYER_HUCKLEBERRY_FINN = @".{2,4}(Tom|Sawyer|Huckleberry|Finn)"
+
+    [<Literal>]
+    let TOM_RIVER = @"Tom.{10,25}river|river.{10,25}Tom"
+
+    [<Literal>]
+    let AZ_ING = @"[a-zA-Z]+ing"
+
+    [<Literal>]
+    let AZ_ING_SPACES = @"\s[a-zA-Z]{0,12}ing\s"
+
+    [<Literal>]
+    let AZ_AWYER_INN = @"([A-Za-z]awyer|[A-Za-z]inn)\s"
+
+    [<Literal>]
+    let QUOTES = @"[""'][^""']{0,30}[?!\.][""']"
+    
+    // Other patterns for Twain
+
+    [<Literal>]
+    let HAVE_THERE = @".*have.*&.*there.*"
+
+    [<Literal>]
+    let HUCK_AZ = @"Huck[A-Za-z]"
+
+    [<Literal>]
+    let AZ_UCK_AZ = @"[A-Za-z]uck[A-Za-z]"
+
+    [<Literal>]
+    let H_AZ_CK_AZ = @"H[A-Za-z]ck[A-Za-z]"
+
+
+
+type BenchmarkConfig() as self =
+    inherit ManualConfig() 
+    do
+        let summaryStyle = SummaryStyle(CultureInfo.InvariantCulture, true, SizeUnit.B, TimeUnit.Millisecond, false)
+                            .WithMaxParameterColumnWidth(60)
+        self.SummaryStyle <- summaryStyle
+        self
+            .AddExporter(CsvExporter(CsvSeparator.Comma, summaryStyle))
+            // .With(CsvExporter(CsvSeparator.Comma, summaryStyle))
+            |> ignore
+
+
+
+[<Config(typedefof<BenchmarkConfig>)>]
+[<MemoryDiagnoser(true)>]
+// [<ShortRunJob>]
+type MatchStartOptimization () =
+    
+
+    [<Params(
+        // Twain regexes
+        
+        Patterns.TWAIN,
+        Patterns.TWAIN_CASEIGNORE,
+        Patterns.AZ_SHING,
+        Patterns.HUCK_SAW,
+        // Patterns.WORD_END,
+        Patterns.AQ_X,
+        Patterns.TOM_SAWYER_HUCKLEBERRY_FINN,
+        Patterns.TOM_SAWYER_HUCKLEBERRY_FINN_CASEIGNORE,
+        Patterns.D02_TOM_SAWYER_HUCKLEBERRY_FINN,
+        Patterns.D24_TOM_SAWYER_HUCKLEBERRY_FINN,
+        Patterns.TOM_RIVER,
+        Patterns.AZ_ING,
+        Patterns.AZ_ING_SPACES,
+        Patterns.AZ_AWYER_INN,
+        Patterns.QUOTES,
+        
+        Patterns.HAVE_THERE,
+        Patterns.HUCK_AZ,
+        Patterns.AZ_UCK_AZ,
+        Patterns.H_AZ_CK_AZ
+        
+        // Rebar regexes
+        
+        // Patterns.SHERLOCK
+        // Patterns.SHERLOCK_CASEIGNORE
+    )>]
+    member val rs: string = "" with get, set
+    
+    member val regex: Regex = Regex("") with get, set
+    
+
+    [<GlobalSetup(Target = "Original")>]
+    member this.OriginalSetup() =
+        this.regex <- Regex(this.rs)
 
     [<Benchmark>]
-    member x.StringIndexOf() =
-        fullInput.IndexOf(prefixString)
+    member this.Original() =
+        this.regex.Count(testInput)
+        
+    
+    member this.ManualTesting() =
+        this.MatchCountTesting()
+    
+    member this.MatchCountTesting() =
+        let pats = [|
+            Patterns.TWAIN // 811
+            Patterns.TWAIN_CASEIGNORE // 965
+            Patterns.AZ_SHING // 1540
+            Patterns.HUCK_SAW // 262
+            // Patterns.WORD_END // Broken in this version
+            Patterns.AQ_X // 4094, Rider finds 4081
+            Patterns.TOM_SAWYER_HUCKLEBERRY_FINN // 2598
+            Patterns.TOM_SAWYER_HUCKLEBERRY_FINN_CASEIGNORE // 4152
+            Patterns.D02_TOM_SAWYER_HUCKLEBERRY_FINN // 2598
+            Patterns.D24_TOM_SAWYER_HUCKLEBERRY_FINN // 1976
+            Patterns.TOM_RIVER // 2
+            Patterns.AZ_ING // 78 423 TODO
+            Patterns.AZ_ING_SPACES // 55 248 TODO
+            Patterns.AZ_AWYER_INN // 209
+            Patterns.QUOTES // 8885, Rider finds 8927
+           
+            Patterns.HAVE_THERE // 426
+            Patterns.HUCK_AZ // 56
+            Patterns.AZ_UCK_AZ // 706
+            Patterns.H_AZ_CK_AZ // 97
+        |]
+        for pat in pats do
+            this.regex <- Regex(pat)
+            let count = this.regex.Count(testInput)
+            ()
+        
 
-    [<Benchmark>]
-    member x.SpanIndexOf() =
-        // about 300x faster when vectorized
-        fullInput.AsSpan().IndexOf(prefixString)
-
-    [<Benchmark>]
-    member x.SpanIndexOf1() =
-        let span = fullInput.AsSpan()
-        let firstSet = prefixString[0]
-        let tail = prefixString.AsSpan(start=1)
-        let mutable currpos = 0
-        let mutable searching = true
-        while searching do
-            // vectorize only to first char
-            let slice = span.Slice(currpos)
-            let delta = slice.IndexOf(firstSet)
-            if delta = -1 then
-                // doesnt exist
-                searching <- false
-            currpos <- currpos + delta
-            let validStart = slice.StartsWith(tail)
-            if validStart then
-                // found
-                searching <- false
-            else currpos <- currpos + 1
-
-
-[<BenchmarkDotNet.Attributes.MemoryDiagnoser>]
-[<ShortRunJob>]
-[<AbstractClass>]
-type SetsPrefix(pattern:string) =
-    let regex = Sbre.Regex(pattern)
-    let matcher = regex.TSetMatcher
-    // find optimized prefix for regex
-    let optimizations =
-        Sbre.Optimizations.findInitialOptimizations
-            (fun node -> matcher.GetOrCreateState(node).Id)
-            (fun node -> matcher.GetOrCreateState(node).Flags)
-            matcher.Cache
-            matcher.ReversePattern
-            matcher.ReverseTrueStarredPattern
-    let prefixSets =
-        match optimizations with
-        | InitialOptimizations.PotentialStartPrefix(prefix) ->
-            let reverseSpan = prefix.Span
-            reverseSpan.Reverse()
-            reverseSpan.ToArray()
-        | InitialOptimizations.SetsPrefix(prefix, transitionid) ->
-            let reverseSpan = prefix.Span
-            reverseSpan.Reverse()
-            reverseSpan.ToArray()
-        | _ -> failwith "could not get prefix"
-
-    let charToTSet (chr:char) = matcher.Cache.Classify(chr)
-    let isElemOfSet (tset1:TSet) (tset2:TSet) = Solver.elemOfSet tset1 tset2
-
-    [<Benchmark>]
-    member x.FirstSetIndexOfTSet() =
-        let inputSpan = fullInput.AsSpan()
-        let mutable currpos = 0
-        let mutable searching = true
-        while searching do
-            let currSet = charToTSet inputSpan[currpos]
-            if isElemOfSet currSet prefixSets[0] then
-                searching <- false
-            else
-                currpos <- currpos + 1
-
-    [<Benchmark>]
-    member x.FirstSetIndexOfChars() =
-        let firstSetChars = matcher.Cache.MintermChars(prefixSets[0])
-        let inputSpan = fullInput.AsSpan()
-        let mutable searching = true
-        while searching do
-            match inputSpan.IndexOfAny(firstSetChars) with
-            | -1 -> failwith "failed search"
-            | n ->
-                searching <- false
-
-    [<Benchmark>]
-    member x.FirstSetIndexOfSearchValues() =
-        let firstSetChars = matcher.Cache.MintermSearchValues(prefixSets[0])
-        let inputSpan = fullInput.AsSpan()
-        let mutable searching = true
-        while searching do
-            match inputSpan.IndexOfAny(firstSetChars) with
-            | -1 -> failwith "failed search"
-            | n ->
-                searching <- false
-
-
-
-
-[<BenchmarkDotNet.Attributes.MemoryDiagnoser>]
-[<ShortRunJob>]
-type Prefix1() =
-    // inherit StringPrefix("Twain")
-    inherit StringPrefix("there")
-
-[<BenchmarkDotNet.Attributes.MemoryDiagnoser>]
-[<ShortRunJob>]
-type Prefix2() =
-    // [HF][ui][cn][kn]
-    inherit SetsPrefix("Huck|Finn")
--- src/Sbre.Test/Program.fs
+++ src/Sbre.Test/Program.fs
@@ -1,41 +1,7 @@
-﻿// module Program = let [<EntryPoint>] main _ = 0
-
+﻿
 module Program
 
-open Sbre
-open Sbre.Benchmarks
-open Sbre.Test
+open Sbre.Benchmarks.Optimizations
 
-// let regex = Regex(@"(abc)+")
-// let a = Regex(@"abc(abc)*")
-// let ab = Regex(@"abcabc")
-// let regex = Regex(@"(?<=_.*)(abc)+(?=.*_)")
-// let pat = Regex(@"ab")
-//
-// let matcher = regex.TSetMatcher
-// // let formalLlmatch = matcher.llmatch("__abc_abc_")
-// let formalLlmatch = matcher.llmatch("__abcabc_abc_")
-//
-
-// let suite = Paper.Twain_1()
-// suite.Setup()
-
-// let suite = Paper.SampleRegexes()
-// let suite = Paper.SampleRegexes()
-// for pat in suite.Patterns do
-//     suite.Pattern <- pat
-//     suite.Setup()
-//
-//     for i = 1 to 100 do
-//         suite.Sbre() |> ignore
-
-
-// let pref = Optimizations.Prefix2()
-// pref.FirstSetIndexOfChars()
-//
-
-let pref = Learning.Learning1()
-pref.Pattern <- pref.Patterns |> Seq.head
-pref.Setup()
-for i = 1 to 50 do
-    pref.Sbre() |> ignore
+let tester = MatchStartOptimization()
+tester.ManualTesting()
